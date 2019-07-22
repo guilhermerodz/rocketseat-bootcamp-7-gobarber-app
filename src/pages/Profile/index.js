16315dc694a66237ae8423b9cfcbeab4a7e211f8
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-22 20:13:51 -0300

    feat: styled routes

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -6,13 +6,13 @@ import Background from '~/components/Background';
 
 // import { Container } from './styles';
 
-export default function Dashboard() {
+export default function Profile() {
   return <Background />;
 }
 
-Dashboard.navigationOptions = {
-  tabBarLabel: 'Agendamentos',
+Profile.navigationOptions = {
+  tabBarLabel: 'Meu perfil',
   tabBarIcon: ({ tintColor }) => (
-    <Icon name="event" size={20} color={tintColor} />
+    <Icon name="person" size={20} color={tintColor} />
   ),
 };

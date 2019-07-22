6e35718144f4e4cb5bd286e04c5e1e18ff8d9cd1
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-22 20:48:47 -0300

    feat: custom navigation bar color for Android

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { AppState } from 'react-native';
 import { useSelector } from 'react-redux';
+
+import changeNavigationBarColor from 'react-native-navigation-bar-color';
 
 import createRouter from './routes';
 
 export default function App() {
+  useEffect(() => {
+    function setAndroidNavigationBar() {
+      changeNavigationBarColor('#8d41a8');
+    }
+
+    function handleAppStateChange(nextAppState) {
+      if (nextAppState === 'active') setAndroidNavigationBar();
+    }
+
+    setAndroidNavigationBar();
+
+    AppState.addEventListener('change', handleAppStateChange);
+    return () => {
+      AppState.removeEventListener('change', handleAppStateChange);
+    };
+  }, []);
+
   const signed = useSelector(state => state.auth.signed);
 
   const Routes = createRouter(signed);

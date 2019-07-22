73afaba708eed135a5a32dc2c42330f251f89e15
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-22 13:55:51 -0300

    chore: now using Reactotron

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,15 @@
+import Reactotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
+
+if (__DEV__) {
+  const tron = Reactotron.configure({ host: '192.168.15.10' })
+    .useReactNative()
+    .use(reactotronRedux())
+    .use(reactotronSaga())
+    .connect();
+
+  tron.clear();
+
+  console.tron = tron;
+}

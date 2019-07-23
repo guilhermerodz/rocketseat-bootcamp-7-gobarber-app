c5ca7b891db6c9415f1731b5f98791a61298bbfa
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-22 21:04:30 -0300

    feat: styled appointment list

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Left, Avatar, Info, Name, Time } from './styles';
+
+export default function Appointment() {
+  return (
+    <Container>
+      <Left>
+        <Avatar
+          source={{ uri: 'http://api.adorable.io/avatar/50/guilhermerodz.png' }}
+        />
+
+        <Info>
+          <Name>Guilherme Rodz</Name>
+          <Time>em 3 horas</Time>
+        </Info>
+      </Left>
+
+      <TouchableOpacity onPress={() => {}}>
+        <Icon name="event-busy" size={20} color="#f64c75" />
+      </TouchableOpacity>
+    </Container>
+  );
+}

e7533a88059b32179d8a114ef5d0e428dc33b7f8
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2019-07-22 23:36:50 -0300

    feat: added DatePicker for Android & iOS

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
+import DateInput from '~/components/DateInput';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function SelectDateTime() {
-  return <Background />;
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <Background>
+      <Container>
+        <DateInput date={date} onChange={setDate} />
+      </Container>
+    </Background>
+  );
 }
 
-SelectDateTime.navigationOptions = {
+SelectDateTime.navigationOptions = ({ navigation }) => ({
   title: 'Selecione o horário',
-};
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#fff" />
+    </TouchableOpacity>
+  ),
+});
